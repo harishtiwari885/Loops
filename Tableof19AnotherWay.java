1f28c1a7921f4da859273cbe2df5329b0ed9494b
harishtiwari885 <harishtiwari7670@gmail.com>
2025-02-26 16:35:17 +0530

    Add files via upload

---
+++ Tableof19AnotherWay.java
@@ -0,0 +1,9 @@
+public class Tableof19AnotherWay{
+    public static void main(String[] args){
+        for(int num = 19 ; num <= 190 ; num++){
+            if(num % 19 == 0){
+                System.out.println(num);
+            }
+        }
+    }
+}
