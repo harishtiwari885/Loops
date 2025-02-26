3d789ba22d92b866f46e6d0afdf7944e8f1b961e
harishtiwari885 <harishtiwari7670@gmail.com>
2025-02-26 16:43:41 +0530

    Add files via upload

---
+++ Table19.java
@@ -0,0 +1,10 @@
+public class Table19{
+    public static void main(String[] args){
+        int a= 19 ;
+        for(int b=1; b<=10 ; b++){
+            int mul =1 ;
+            mul= a*b;
+            System.out.println(a + "x" + b + "=" + mul);
+        }
+    }
+}
