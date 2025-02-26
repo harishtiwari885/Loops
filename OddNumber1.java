36c44cdc3edcc9e80cd195492052880890e44537
harishtiwari885 <harishtiwari7670@gmail.com>
2025-02-26 16:13:59 +0530

    Add files via upload

---
+++ OddNumber1.java
@@ -0,0 +1,9 @@
+public class OddNumber1{
+    public static void main(String[] args){
+        for(int num = 1 ; num <= 100 ; num++){
+            if(num % 2 !=0){
+                System.out.println(num);
+            }
+        }
+    }
+}
---
+++ OddNumber2.java
@@ -0,0 +1,7 @@
+public class OddNumber2{
+    public static void main(String[] args){
+        for(int num = 1 ; num <= 100 ; num +=2){
+                System.out.println(num);
+        }
+    }
+}
